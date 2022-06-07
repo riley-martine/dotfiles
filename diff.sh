566c355118d6b234e020fa73d42e870794795ccd
Riley Martine <riley.martine@protonmail.com>
2022-06-06 23:56:31 -0600

    Update git config

--- diff.sh
+++ diff.sh
@@ -6,6 +6,8 @@ declare -A dotfiles=(
     ["$HOME/.tmuxline"]="tmux/.tmuxline"
     ["$HOME/.vim/vimrc"]="vim/vimrc"
     ["$HOME/.config/starship.toml"]="starship/starship.toml"
+    ["$HOME/.gitconfig"]="git/.gitconfig"
+    ["$HOME/.gitmessage"]="git/.gitmessage"
 )
 
 for i in "${!dotfiles[@]}"; do
