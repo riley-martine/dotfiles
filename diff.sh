74cfdcd49c043a6d2425de3c358ac6e497d5cf51
Riley Martine <riley.martine@protonmail.com>
2022-12-03 13:13:26 -0700

    Edit git config

--- diff.sh
+++ diff.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-declare -A dotfiles=(
+declare -A dotfiles
+dotfiles=(
     ["$HOME/.tmux.conf"]="tmux/.tmux.conf"
     ["$HOME/.tmuxline"]="tmux/.tmuxline"
+
     ["$HOME/.vim/vimrc"]="vim/vimrc"
+
     ["$HOME/.config/starship.toml"]="starship/starship.toml"
+
     ["$HOME/.gitconfig"]="git/.gitconfig"
-    ["$HOME/.gitmessage"]="git/.gitmessage"
+    ["$HOME/.config/git/identity.gitconfig"]="git/identity.gitconfig"
+    ["$HOME/.config/git/gitmessage"]="git/gitmessage"
+    ["$HOME/.config/git/gitignore"]="git/gitignore"
+
     ["$__fish_config_dir/fish_plugins"]="fish/fish_plugins"
     ["$__fish_config_dir/config.fish"]="fish/config.fish"
 )
 
 for i in "${!dotfiles[@]}"; do
-    diff "$i" "${dotfiles[$i]}"
+    PAGER='cat' delta "$i" "${dotfiles[$i]}"
 done
 
 fish installed.fish
