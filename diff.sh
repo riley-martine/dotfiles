66c5642508f9df24f88dd1a6b7f70de055eb1250
Riley Martine <riley.martine@protonmail.com>
2022-12-26 13:06:11 -0700

    Add update system to diff

--- diff.sh
+++ diff.sh
@@ -3,28 +3,29 @@ set -uo pipefail
 
 declare -A dotfiles
 dotfiles=(
-    ["$HOME/.tmux.conf"]="tmux/.tmux.conf"
-    ["$HOME/.config/tmux/tokyonight_day.tmux"]="tmux/tokyonight_day.tmux"
+     ["$HOME/.tmux.conf"]="tmux/.tmux.conf"
+     ["$HOME/.config/tmux/tokyonight_day.tmux"]="tmux/tokyonight_day.tmux"
 
-    ["$HOME/.vim/vimrc"]="vim/vimrc"
+     ["$HOME/.vim/vimrc"]="vim/vimrc"
 
-    ["$HOME/.config/starship.toml"]="starship/starship.toml"
+     ["$HOME/.config/starship.toml"]="starship/starship.toml"
 
-    ["$HOME/.gitconfig"]="git/.gitconfig"
-    ["$HOME/.config/git/identity.gitconfig"]="git/identity.gitconfig"
-    ["$HOME/.config/git/tokyonight_day.gitconfig"]="git/tokyonight_day.gitconfig"
-    ["$HOME/.config/git/gitmessage"]="git/gitmessage"
-    ["$HOME/.config/git/gitignore"]="git/gitignore"
+     ["$HOME/.gitconfig"]="git/.gitconfig"
+     ["$HOME/.config/git/identity.gitconfig"]="git/identity.gitconfig"
+     ["$HOME/.config/git/tokyonight_day.gitconfig"]="git/tokyonight_day.gitconfig"
+     ["$HOME/.config/git/gitmessage"]="git/gitmessage"
+     ["$HOME/.config/git/gitignore"]="git/gitignore"
 
-    ["$HOME/.config/bat/themes/tokyonight_day.tmTheme"]="bat/tokyonight_day.tmTheme"
+     ["$HOME/.config/bat/themes/tokyonight_day.tmTheme"]="bat/tokyonight_day.tmTheme"
 
-    ["$HOME/.config/fish/fish_plugins"]="fish/fish_plugins"
-    ["$HOME/.config/fish/config.fish"]="fish/config.fish"
-    ["$HOME/.config/fish/themes/tokyonight_day.fish"]="fish/themes/tokyonight_day.fish"
+     ["$HOME/.config/fish/fish_plugins"]="fish/fish_plugins"
+     ["$HOME/.config/fish/config.fish"]="fish/config.fish"
+     ["$HOME/.config/fish/themes/tokyonight_day.fish"]="fish/themes/tokyonight_day.fish"
 
-    ["$HOME/Documents/Tokyonight\ Day.terminal"]="terminal.app/Tokyonight\ Day.terminal"
+     ["$HOME/Documents/Tokyonight\ Day.terminal"]="terminal.app/Tokyonight\ Day.terminal"
 
-    ["$HOME/bin/random-words"]="bin/random-words"
+     ["$HOME/bin/random-words"]="bin/random-words"
+     ["$HOME/bin/update-system"]="bin/update-system"
 )
 
 for i in "${!dotfiles[@]}"; do
