90ebe5e10a2fc7d1601e8a38b309eefefb067fac
Riley Martine <riley.martine@protonmail.com>
2023-10-10 17:40:35 -0600

    "unsupported" use

--- update.d/pre-commit.sh
+++ update.d/pre-commit.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+# pre-commit refuses to update any config unless you're currently in a repo
+TMP_DIR="$(mktemp -d)"
+trap 'rm -rf $TMP_DIR' EXIT
+cd "$TMP_DIR"
+git init
+
 pre-commit gc
 for conf in ~/.config/pre-commit/*.yaml; do
     pre-commit autoupdate --config "$conf" --jobs 5
